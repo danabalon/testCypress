82717c9d228c14c521e183f8fa96aa2988a8363b
Anaba <diego.anabalon@fidtech.net>
2019-10-11 16:02:07 -0300

    default configuration project

---
+++ cypress/integration/sample_spec.js
@@ -0,0 +1,14 @@
+
+
+
+describe('My First Test', function() {
+  it('Does not do much 1!', function() {
+    expect(true).to.equal(true)
+  })
+});
+
+describe('My First Test', function() {
+  it('Does not do much!', function() {
+    expect(true).to.equal(false)
+  })
+})
